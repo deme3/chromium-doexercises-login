ab0e46fff5dcf0bb0ce448984d3123f313da12ee
Demetrio Battaglia <deme3.iot@gmail.com>
2021-05-07 14:24:58 +0200

    More aggressive login attempt policy plus minor refactoring

--- content-scripts/content.js
+++ content-scripts/content.js
@@ -2,20 +2,26 @@ function isundefined(x) { return typeof x === "undefined"; }
 
 function isempty(x) { return x.trim() === ""; }
 
-var failInterval = null;
+var failInterval = null,
+    failures = 5;
+const ATTEMPTS_INTERVAL = 500;
 window.addEventListener("load", () => {
     // spaghetti
+    console.debug("DoExercises AutoLogin attached successfully to onLoad Event");
     if (document.title === "DoExercises - PS") {
-        var emailInput = document.getElementById("email_address");
-        var studentIdInput = document.getElementById("matricola");
-        var passwordInput = document.getElementById("password");
-        var submitButton = document.getElementById("signin");
+        console.debug(`DoExercises AutoLogin triggered: initiating repeating interval for`, 1 / ATTEMPTS_INTERVAL * 1000, `attempts per second`);
 
-        chrome.runtime.sendMessage({ action: "getStorage" }, function(response) {
-            var hasFailed = true;
-            var failures = 5;
+        failInterval = setInterval(() => {
+            console.debug(`DoExercises AutoLogin: Attempt to login #${5 - failures + 1}`)
+            var emailInput = document.getElementById("email_address");
+            var studentIdInput = document.getElementById("matricola");
+            var passwordInput = document.getElementById("password");
+            var submitButton = document.getElementById("signin");
 
-            failInterval = setInterval(() => {
+            chrome.runtime.sendMessage({ action: "getStorage" }, function(response) {
+                let decoy = { student_id: response.student_id, username: response.username };
+                console.debug("DoExercises Storage Worker's response", decoy);
+
                 if (!isundefined(response.username) && !isundefined(response.student_id) && !isundefined(response.password) &&
                     !isempty(response.username) && !isempty(response.student_id) && !isempty(response.password) &&
                     emailInput && studentIdInput && passwordInput) {
@@ -24,13 +30,34 @@ window.addEventListener("load", () => {
                     passwordInput.value = response.password;
                     submitButton.click();
                     clearInterval(failInterval);
+                    console.debug("%cLogin attempt successful", "color: green");
                 } else {
+                    console.debug("%cLogin failed", "color: red");
+                    console.debug({
+                        domComponents: {
+                            email: emailInput,
+                            studentId: studentIdInput,
+                            password: passwordInput,
+                            submit: submitButton
+                        },
+                        conditions: [
+                            { condition: "Is response.username defined?", result: !isundefined(response.username) },
+                            { condition: "Is response.student_id defined?", result: !isundefined(response.student_id) },
+                            { condition: "Is response.password defined?", result: !isundefined(response.password) },
+                            { condition: "Is response.username empty?", result: isempty(response.username) },
+                            { condition: "Is response.student_id empty?", result: isempty(response.student_id) },
+                            { condition: "Is response.password empty?", result: isempty(response.password) }
+                        ],
+                        workerResponse: decoy
+                    });
                     if (failures > 0)
                         failures--;
-                    else
+                    else {
                         clearInterval(failInterval);
+                        failures = 5;
+                    }
                 }
-            }, 500);
-        });
+            });
+        }, ATTEMPTS_INTERVAL);
     }
 });
